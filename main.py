6acfc9bc7033d1970621adb520b7605cd05f548e
Уваров Илья <popodoss@mail.ru>
2022-10-31 11:22:22 +0300

    dz modul

---
+++ main.py
@@ -0,0 +1,13 @@
+from application import salary
+from application.db import people
+import datetime
+
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+    # print(datetime.datetime.now())
+    date = datetime.datetime.now()
+    print(date)
+
+
+
